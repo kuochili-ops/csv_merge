be0f4c2324ac55111cc814ac6721b567ee071f6c
kuochili-ops <kuochili@gmail.com>
2025-11-26 15:49:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import streamlit as st
 import pandas as pd
 
 base_df = pd.read_csv("mount_2024.csv")
 add_df = pd.read_csv("Price_ATC_S.csv")
 
-print("mount_2024.csv 欄位：", base_df.columns.tolist())
-print("Price_ATC_S.csv 欄位：", add_df.columns.tolist())
-print("共同欄位：", set(base_df.columns) & set(add_df.columns))
+st.write("mount_2024.csv 欄位：", base_df.columns.tolist())
+st.write("Price_ATC_S.csv 欄位：", add_df.columns.tolist())
+st.write("共同欄位：", set(base_df.columns) & set(add_df.columns))
